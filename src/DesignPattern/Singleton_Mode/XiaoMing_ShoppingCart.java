443829fd7d3d08d014058f8ff046499aa216841c
baizhu0414 <15156699185@163.com>
2025-03-25 20:24:46 +0800

    01-单例模式

---
+++ src/DesignPattern/Singleton_Mode/XiaoMing_ShoppingCart.java
@@ -0,0 +1,58 @@
+package DesignPattern.Singleton_Mode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// 【设计模式专题之单例模式】1.小明的购物车
+//  https://kamacoder.com/problempage.php?pid=1074
+class Obj {
+    String str;
+    int val;
+
+    Obj(String str, int val) {
+        this.str = str;
+        this.val = val;
+    }
+
+    @Override
+    public String toString() {
+        return str + " " + val;
+    }
+
+}
+
+public class XiaoMing_ShoppingCart {
+    private static XiaoMing_ShoppingCart instance;
+    List<Obj> cart_list;
+
+    // 懒汉模式+双重检验锁
+    public static synchronized XiaoMing_ShoppingCart getInstance() {
+        if (instance == null) {
+            synchronized(XiaoMing_ShoppingCart.class) {
+                if (instance == null) {
+                    instance = new XiaoMing_ShoppingCart();
+                }
+            }
+        }
+        return instance;
+    }
+
+    // 私有构造函数
+    private XiaoMing_ShoppingCart() {
+    }
+
+    public boolean add(Obj obj) {
+        if (cart_list == null) {
+            cart_list = new ArrayList<>();
+        }
+        return cart_list.add(obj);
+    }
+
+    public void PrintCartList() {
+        for (int i = 0; i < cart_list.size(); i++) {
+            System.out.println(cart_list.get(i).toString());
+        }
+    }
+
+}
+
---
+++ src/DesignPattern/Singleton_Mode/test.java
@@ -0,0 +1,19 @@
+package DesignPattern.Singleton_Mode;
+
+import java.util.Scanner;
+
+public class test {
+    public static void main(String []args) {
+        Scanner sc = new Scanner(System.in);
+        XiaoMing_ShoppingCart instance = XiaoMing_ShoppingCart.getInstance();
+
+        for(int i=0; i<3; i++) {
+            String str = sc.next();
+            int val = sc.nextInt();
+            instance.add(new Obj(str, val));
+        }
+        instance.PrintCartList();
+
+        sc.close();
+    }
+}
